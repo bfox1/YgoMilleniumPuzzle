d98bb6d2b14cf6b3eb3a887c903d847e2b739e42
bfox1 <MylilPrincess5413>
2019-02-03 14:03:50 -0800

    Added Tile objects Plus Documentations

---
+++ src/io/github/bfox1/ygomp/application/Tile.java
@@ -0,0 +1,15 @@
+package io.github.bfox1.ygomp.application;
+
+/**
+ * The Tile Class is the entity represented as "tiles" or "Squares" to which the player must move
+ * on the screen to get 3 or more in a row either horizontal or vertical.
+ *
+ * Tile Will contain coordinates that are matched with the playing field. The playing field per player is
+ * 6 Wide and 12 Tall
+ *
+ * Each Column with be labeled by a number and will be its own Tile Array. The Array goes from 0-5.
+ */
+public class Tile
+{
+
+}
